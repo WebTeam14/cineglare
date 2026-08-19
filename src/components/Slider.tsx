d1328b5479f154302b3947721e03342222c902d1
TETPL <149654389+WebTeam14@users.noreply.github.com>
2026-08-19 18:25:33 +0530

    Partners: remove side edge fade overlays on marquee

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -72,15 +72,6 @@ const PartnerSlider = () => {
       </div>
 
       <div className="relative space-y-5">
-        <div
-          aria-hidden
-          className="pointer-events-none absolute inset-y-0 left-0 z-10 w-16 bg-gradient-to-r from-black to-transparent sm:w-24"
-        />
-        <div
-          aria-hidden
-          className="pointer-events-none absolute inset-y-0 right-0 z-10 w-16 bg-gradient-to-l from-black to-transparent sm:w-24"
-        />
-
         <MarqueeRow items={rowA} duration={70} />
         <MarqueeRow items={rowB} reverse duration={85} />
       </div>
