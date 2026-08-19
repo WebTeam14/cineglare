cd0c8b8ba98c4e40f30e96c684bc8a0ec3885752
WebTeam14 <web.technoriya@gmail.com>
2026-08-19 16:21:13 +0530

    logo updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { cn } from "@/lib/utils";
-import logo from "@/assets/logo/Cineglare-light.svg";
+import logo from "@/assets/logo/Cineglare.svg";
 
 const navLinks = [
   { name: "Home", path: "/" },
