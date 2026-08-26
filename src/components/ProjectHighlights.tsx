d369a98173f0232299d47f03750c2159ba3da0cc
Shubhasmita <shubhasmita20.sahu@gmail.com>
2026-08-26 18:08:45 +0530

    updated images

--- src/components/ProjectHighlights.tsx
+++ src/components/ProjectHighlights.tsx
@@ -1,6 +1,6 @@
 import { Reveal, Stagger } from "@/components/motion/Reveal";
 import project1 from "@/assets/images/H-Prjt1.jpg";
-import project2 from "@/assets/images/H-Prj2.png";
+import project2 from "@/assets/images/H-Prjt2.jpg";
 import project3 from "@/assets/images/H-Prjt3.jpg";
 
 const ProjectHighlights = () => {
